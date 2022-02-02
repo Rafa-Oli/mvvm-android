5ce0b2c8cacce56542138bb3165d539c6f3dc880
Rafaela Santos <rafaela.santos@trizy.com.br>
2022-02-01 22:07:03 -0300

    feat: create repository and add action button

--- app/src/main/java/com/example/mvvm/MainActivity.kt
+++ app/src/main/java/com/example/mvvm/MainActivity.kt
@@ -2,22 +2,38 @@ package com.example.mvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.viewModels
-import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.Observer
 
 class MainActivity : AppCompatActivity() {
 
-
-//    private lateinit var viewModel: MainViewModel
-    private val viewmodel: MainViewModel by viewModels();
+    private val viewModel: MainViewModel by viewModels();
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val text = findViewById<TextView>(R.id.textWelcome)
+        val buttonLogin = findViewById<TextView>(R.id.buttonLogin);
+        val editName = findViewById<TextView>(R.id.editName);
 
-//        viewModel = new ViewModelProvider(this).get(MainViewModel::class.java)
+        viewModel.textWelcome.observe(this, Observer {
+            text.text = it
+        });
 
+        viewModel.login.observe(this, Observer {
+            if (it) {
+                Toast.makeText(this, "Sucesso!", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Falha!", Toast.LENGTH_SHORT).show()
+            }
+        });
 
-
+        buttonLogin.setOnClickListener {
+            println("entrou button");
+            var login = editName.text.toString()
+            viewModel.login(login)
+        }
     }
 }
