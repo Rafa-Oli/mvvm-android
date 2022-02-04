81ba419b3cb772d21772320ccd740a54cc396f48
Rafaela Santos <rafaela.santos@trizy.com.br>
2022-02-04 20:14:10 -0300

    refactor: refactor classes

--- app/src/main/java/com/example/mvvm/MainActivity.kt
+++ app/src/main/java/com/example/mvvm/MainActivity.kt
@@ -1,38 +1,50 @@
 package com.example.mvvm
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.lifecycle.Observer
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
-    private val viewModel: MainViewModel by viewModels();
+    private val mMainViewModel: MainViewModel by viewModels();
+    private lateinit var mContext: Context
+    private lateinit var editName: TextView
+    private lateinit var buttonLogin: TextView
+    private lateinit var textWelcome: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val text = findViewById<TextView>(R.id.textWelcome)
-        val buttonLogin = findViewById<TextView>(R.id.buttonLogin);
-        val editName = findViewById<TextView>(R.id.editName);
 
-        viewModel.textWelcome.observe(this, Observer {
-            text.text = it
+        textWelcome = findViewById<TextView>(R.id.textWelcome)
+        buttonLogin = findViewById<TextView>(R.id.buttonLogin);
+        editName = findViewById<TextView>(R.id.editName);
+        this.mContext = this
+
+
+        buttonLogin.setOnClickListener(this)
+
+        this.createObservers()
+    }
+
+
+    override fun onClick(view: View?) {
+        val name = editName.text.toString()
+        this.mMainViewModel.doLogin(name)
+    }
+
+    private fun createObservers() {
+        mMainViewModel.welcome().observe(this, Observer {
+            textWelcome.text = it
         });
 
-        viewModel.login.observe(this, Observer {
-            if (it) {
-                Toast.makeText(this, "Sucesso!", Toast.LENGTH_SHORT).show()
-            } else {
-                Toast.makeText(this, "Falha!", Toast.LENGTH_SHORT).show()
-            }
+        mMainViewModel.login().observe(this, Observer {
+            Toast.makeText(mContext, it, Toast.LENGTH_SHORT).show()
         });
-
-        buttonLogin.setOnClickListener {
-            var login = editName.text.toString()
-            viewModel.login(login)
-        }
     }
 }
--- app/src/main/java/com/example/mvvm/MainViewModel.kt
+++ app/src/main/java/com/example/mvvm/MainViewModel.kt
@@ -1,22 +1,31 @@
 package com.example.mvvm
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
-    private var mRepository = PersonRepository()
+    private var mPersonRepository = PersonRepository()
     private var mTextWelcome = MutableLiveData<String>();
-    var textWelcome = mTextWelcome
+    private var mToastNotification = MutableLiveData<String>();
 
-    private var mLogin = MutableLiveData<Boolean>()
-    var login = mLogin
 
     init {
         mTextWelcome.value = "Hello World";
     }
 
-    fun login(login: String) {
-        val ret = mRepository.login(login)
-        mLogin.value = ret
+    fun welcome(): LiveData<String> {
+        return mTextWelcome
+    }
+
+    fun login(): LiveData<String> {
+        return mToastNotification
+    }
+
+    fun doLogin(email: String) {
+        if (this.mPersonRepository.login(email))
+            mToastNotification.setValue("Success")
+        else
+            mToastNotification.setValue("Falha")
     }
 }
